3fbe5ce300ee181553013c3f835698a27022a730
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-10-16 22:12:40 +0300

    feat: task1 complete

---
+++ task1/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        Integer[] arr = convertLineToIntArray(line);
+
+        Integer[] subArr = new Integer[] {0, 0, 0};
+
+        for (int i : arr) {
+            subArr[i - 1] = subArr[i - 1] + 1;
+        }
+
+        int i = 0;
+        int j = 0;
+        int repeat = subArr[j];
+        while (repeat != 0) {
+            arr[i] = j + 1;
+            i++;
+            repeat--;
+
+            if (repeat == 0 && j < subArr.length - 1) {
+                j++;
+                repeat = subArr[j];
+            }
+        }
+
+        for (int num : arr) {
+            System.out.print(num);
+        }
+    }
+
+    public static Integer[] convertLineToIntArray(String line) {
+        String[] chars = line.split("\\+");
+        Integer[] resultArr = new Integer[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            resultArr[i] = Integer.parseInt(chars[i]);
+        }
+        return resultArr;
+    }
+}
