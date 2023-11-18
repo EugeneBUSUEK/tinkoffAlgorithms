3b67ef6101364af37933e419a8e1b2fd5ca49df2
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-11-18 18:30:32 +0300

    feat: unsolvable time limit exceeded task20

--- task20/src/Main.java
+++ task20/src/Main.java
@@ -16,24 +16,26 @@ public class Main {
     private static void task(int n) {
         double d = 0.0;
         int a = 2;
-        int b = 0;
-        int c;
-        while (true) {
-            if (n % a == 0) {
-                b = n / a;
-            } else {
+        int b = 3;
+        while (a < n / 2 && b < (n / a) / 2) {
+            if (n % a != 0) {
                 a++;
                 continue;
             }
-            if (n % (a * b) == 0) {
-                c = n / (a * b);
-                if (c != 1 && c != a && c != b) {
-                    System.out.println("YES");
-                    System.out.println(a + " " + b + " " + c);
-                }
+            if ((n / a) % b != 0 || (n / a) / b == a || b == a || (n / a) / b == 1) {
+                b++;
+                continue;
+            }
+            if (n % (a * b) == 0 && n / (a * b) != a && n / (a * b) != b && n / (a * b) != 1) {
+                System.out.println("YES");
+                System.out.println(a + " " + b + " " + n / (a * b));
+                return;
+            } else {
+                break;
             }
 
         }
+        System.out.println("NO");
     }
 
     public static Integer[] convertLineToLongArray(String line) {
